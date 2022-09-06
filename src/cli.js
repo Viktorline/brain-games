8dd120bb5ce7d90f469c29ba223ce29e0410b985
Victor Murzin <viktorlinework@gmail.com>
2022-09-06 09:53:28 +0700

    add eslint, edit errors, add codeclimate badge

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 function welcome() {
-  console.log("Welcome to the Brain Games!");
-  const playerName = readlineSync.question("May I have your name? ");
+  console.log('Welcome to the Brain Games!');
+  const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
 }
 
