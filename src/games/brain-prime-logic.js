19817fb2ce47cc82172a635aa67c18365e666314
Victor Murzin <viktorlinework@gmail.com>
2022-09-08 09:37:06 +0700

    add brain-prime

---
+++ src/games/brain-prime-logic.js
@@ -0,0 +1,36 @@
+import { getRandom } from '../functions.js';
+import startGame from '../index.js';
+
+const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function isPrime(number) {
+  if (number < 2) {
+    return 'no';
+  }
+  if (number === 2) {
+    return 'yes';
+  }
+
+  let i = 2;
+  const limit = Math.sqrt(number);
+  while (i <= limit) {
+    if (number % i === 0) {
+      return 'no';
+    }
+    i += 1;
+  }
+
+  return 'yes';
+}
+
+function newRound() {
+  const question = getRandom(1, 100);
+  const answer = isPrime(question);
+  return [question, answer];
+}
+
+function brainPrime() {
+  startGame(gameRule, newRound);
+}
+
+export default brainPrime;
